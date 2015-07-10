7da7b2901f2bc84d3eea1a5d38c63725f7119147
artuzam <charliecazc@gmail.com>
2015-07-09 21:20:57 -0600

    Emplantillar
    Se emplantillan las clases arbol y lista

--- ProyectoFinal/Arbol.h
+++ ProyectoFinal/Arbol.h
@@ -1,22 +1,75 @@
-
 #pragma once
 
 #include "Elemento.h"
 #include "Operador.h"
 #include "Operacion.h"
 
+template <class T>
 class Arbol {
+
+	template <class T>
+
 private:
-	Elemento * raiz;
-	
-	void destruirRec(Elemento *);
-	void solucionarRec(Elemento *&);
-	void descomponerRec(Elemento *&);
+
+	Arbol(Elemento*){
+		Elemento * raiz;
+	}
+	void destruirRec(Elemento *){
+		if (actual->getHijoIzquierdo() != NULL) {
+			destruirRec(actual->getHijoIzquierdo());
+		}
+		if (actual->getHijoDerecho() != NULL) {
+			destruirRec(actual->getHijoDerecho());
+		}
+		delete actual;
+	}
+
+	void solucionarRec(Elemento *&){
+		Elemento * izq = actual->getHijoIzquierdo();
+		Elemento * der = actual->getHijoDerecho();
+		Operador * op = dynamic_cast<Operador*>(actual);
+		if (op != NULL) {
+			if (izq != NULL) {
+				solucionarRec(actual->getHijoIzquierdo());
+			}
+			if (der != NULL) {
+				solucionarRec(actual->getHijoDerecho());
+			}
+			Elemento * solucion = op->operar(actual->getHijoIzquierdo(), actual->getHijoDerecho());
+			delete actual;
+			actual = solucion;
+		}
+	}
+
+	void descomponerRec(Elemento *&){
+		Operacion * operacion = dynamic_cast<Operacion *>(actual);
+		if (operacion != NULL) {
+
+			Elemento * tmp = actual;
+			actual = operacion->descomponer();
+			delete tmp;
+
+			Elemento * izq = actual->getHijoIzquierdo();
+			Elemento * der = actual->getHijoDerecho();
+			if (izq != NULL) {
+				descomponerRec(actual->getHijoIzquierdo());
+			}
+			if (der != NULL) {
+				descomponerRec(actual->getHijoDerecho());
+			}
+		}
+	}
 
 public:
-	Arbol(Elemento *);
-	~Arbol();
 
-	void descomponer();
-	Elemento * solucionar();
+	~Arbol(){
+
+	}
+
+	void descomponer(){
+		descomponerRec(raiz);
+	}
+	Elemento * solucionar(){
+		solucionarRec(raiz);
+	}
 };
--- ProyectoFinal/Lista.h
+++ ProyectoFinal/Lista.h
@@ -1,13 +1,14 @@
-
 #pragma once
 
 #include "Elemento.h"
 #include "Operacion.h"
-
 using namespace std;
+
+template <class T>
 
 class Lista {
 
+	template <class T>
 	friend ostream& operator<<(ostream&, Lista&);
 
 private:
@@ -15,14 +16,57 @@ private:
 	int cantidadElementos;
 
 public:
-	Lista();
-	Lista(istream &);
-	~Lista();
+	Lista(){
+		this->cabeza = NULL;
+		this->cola = NULL;
+	}
 
-	void insertar(Elemento *);
-	int getCantidadElementos();
-	Elemento * getCopy(int);
+	Lista(istream &){
+		this->cabeza = NULL;
+		this->cola = NULL;
+	}
+	~Lista(){
+		Elemento * actual = cabeza;
+		while (actual != NULL) {
+			Elemento * tmp = actual;
+			actual = actual->getProximo();
+			delete tmp;
+		}
+	}
+
+
+	void insertar(Elemento *nuevoelemento){
+		if (cabeza == NULL) {
+			this->cabeza = nuevoElemento;
+			this->cola = nuevoElemento;
+		}
+		else {
+			this->cola->setProximo(nuevoElemento);
+			this->cola = nuevoElemento;
+		}
+		cantidadElementos++;
+	}
+	int getCantidadElementos(){
+		return cantidadElementos;
+	}
+
+	Elemento * getCopy(int){
+		Elemento * actual = cabeza;
+		while (actual != NULL && i != 0) {
+			actual = actual->getProximo();
+			i--;
+		}
+		return actual->clonar();
+	}
+
+
+	ostream& operator<<(ostream & out, Lista & lista) {
+		Elemento * actual = lista.cabeza;
+		int c = 0;
+		while (actual != NULL) {
+			out << ++c << ". " << *actual << endl;
+			actual = actual->getProximo();
+		}
+		return out << endl;
+	}
 };
-
-ostream& operator<<(ostream&, Lista&);
-
